edf0021cbd32b4d22a0e6ced0d0b23dd67ea7e1d
Deepak Kumar Sharma <deepak.khandal3@gmail.com>
2014-04-23 11:22:52 +0530

    new change

--- www/js/index.js
+++ www/js/index.js
@@ -55,8 +55,38 @@ var app = {
         console.log('Received Event: ' + id);
     },
     
-    take_pic1: function() {
-        alert("take_pic()1");
+    // api-camera
+    onPhotoDataSuccess: function(imageData) {
+        console.log("* * * onPhotoDataSuccess");
+        var cameraImage = document.getElementById('cameraImage');
+        cameraImage.style.visibility = 'visible';
+        cameraImage.src = "data:image/jpeg;base64," + imageData;
+    },
+    
+    onPhotoURISuccess: function(imageURI) {
+        console.log("* * * onPhotoURISuccess");
+        // Uncomment to view the image file URI 
+        // console.log(imageURI);
+        var cameraImage = document.getElementById('cameraImage');
+        cameraImage.style.visibility = 'visible';
+        cameraImage.src = imageURI;
+    },
+    
+    take_pic: function() {
+        alert("take_pic");
+        navigator.camera.getPicture(onPhotoDataSuccess, function(ex) {
+            alert("Camera Error!");
+        }, { quality : 30, destinationType: destinationType.DATA_URL });
+    },
+    
+    album_pic: function() { 
+        navigator.camera.getPicture(onPhotoURISuccess, function(ex) {
+                alert("Camera Error!"); }, 
+                { quality: 30, 
+            destinationType: destinationType.FILE_URI,
+            // Android Quirk: Camera.PictureSourceType.PHOTOLIBRARY and 
+            // Camera.PictureSourceType.SAVEDPHOTOALBUM display the same photo album.
+            sourceType: pictureSource.SAVEDPHOTOALBUM });
     }
 
 };
